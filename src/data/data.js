4a778f8b69cf8687cbbd5b750b6195e283242b36
Orlando Alvarez <oalvarezalarcon0304@gmail.com>
2024-09-12 21:42:09 -0400

    added basic data

---
+++ src/data/data.js
@@ -0,0 +1,86 @@
+const prehistory = [
+  { 
+    title: 'Gatherers',
+    description: 'People who collected fruits, nuts, berries, roots, and other edible plants from their surroundings. They knew which plants were safe to eat and which were not, and they understood the seasons when different plants were available. Also, they provided a reliable source of food and was crucial for the survival of early human communities'
+  },
+  { 
+    title: 'Hunters',
+    description: 'They were responsible for finding and killing animals for food. They used simple tools like spears, bows, and arrows to hunt animals such as deer, mammoths, and wild boars. This trade required skill, strength, and teamwork, and it provided not just food, but also materials like bones for tools and fur for clothing. This was essential for the survival of early humans, especially in harsh climates where plant-based food was scarce'
+  },
+  { 
+    title: 'Agriculturalists',
+    description: 'They were among the first to practice agriculture by planting and cultivating crops like wheat, barley, and legumes. This shift from merely gathering to growing food allowed human communities to settle in one place rather than constantly moving in search of food. The development of agriculture enabled early humans to produce their own food, leading to more stable food supplies and the growth of permanent settlements.'
+  }
+];
+
+const ancientCivilizations = [
+  { 
+    title: 'Scribes',
+    description: 'In civilizations like Egypt, Mesopotamia, and China, there were highly skilled individuals trained in the art of writing and maintaining detailed records. Their work involved documenting laws, religious texts, trade transactions, and significant historical events. Since few people possessed this skill, their role was crucial for administration, communication, and the preservation of knowledge, making them highly respected within their communities.'
+  },
+  { 
+    title: 'Architects',
+    description: 'There were experts who specialized in designing and constructing monumental structures, such as the pyramids in Egypt, ziggurats in Mesopotamia, and temples in ancient Greece and Rome. These individuals, knowledgeable in engineering, mathematics, and materials, created works that served religious, political, or social purposes while symbolizing the power and culture of their society. Their advanced technical expertise enabled the creation of architectural wonders that still amaze us today.'
+  },
+  { 
+    title: 'Traders',
+    description: 'In many ancient civilizations, some individuals were dedicated to facilitating trade and cultural exchange between different regions. They were responsible for exchanging goods such as spices, textiles, metals, pottery, and luxury items across extensive land and sea routes. These activities not only boosted local economies but also promoted the spread of ideas, technologies, and customs among various cultures. Those who excelled in this field often achieved wealth and influence.'
+  },
+  { 
+    title: 'Farmers',
+    description: 'In ancient societies, certain people were responsible for cultivating the land to produce essential food supplies that sustained the population. They grew crops such as wheat, barley, rice, and maize, and domesticated animals for meat, milk, and labor, depending on the region. The surplus they produced enabled the growth of cities, supported specialized trades, and allowed for social and political development, serving as a foundation for the organization of societies.'
+  },
+];
+
+const medieval = [
+  { 
+    title: 'Blacksmith',
+    description: 'These individuals were skilled in working with iron and steel to create various tools, weapons, armor, horseshoes, and household items. Their work was crucial for both everyday life and warfare. The craftsmanship required both physical strength and technical knowledge, making them highly valuable in both rural and urban settings.'
+  },
+  { 
+    title: 'Knight',
+    description: 'Belonging to the noble class, these warriors served their lords and kings as heavily armored fighters. They adhered to a code of conduct emphasizing bravery, honor, and service. This role required years of rigorous training, often starting from childhood, and played a vital part in the defense of territories, feudal administration, and in battles and tournaments.'
+  },
+  { 
+    title: 'Monk',
+    description: 'Devoted to serving God and the Church, these men and women lived in monasteries and convents. They were involved in copying manuscripts, teaching, healing, and providing charity to the poor. Their commitment to spiritual life had a significant influence on education, medicine, and social services during the medieval period.'
+  },
+  { 
+    title: 'Alchemist',
+    description: 'Known for their work in early forms of chemistry, these practitioners sought to transform base metals into gold and discover the elixir of life. They experimented with various substances and processes, combining elements of science, philosophy, and mysticism. Their efforts laid the groundwork for modern chemistry and medicine, and they were often sought after by nobility and monarchs for their supposed ability to create wealth and cure diseases.'
+  },
+  { 
+    title: 'Cartographer',
+    description: 'Individuals who specialized in creating maps and charts for navigation, exploration, and territorial management. Their work was crucial for traders, explorers, and military leaders, providing detailed depictions of geographical areas, trade routes, and political boundaries. Given the lack of precise tools, this profession required a combination of art, science, and knowledge of mathematics and geography, making them highly valued for expanding knowledge of the world.'
+  }
+];
+
+const industrialRevolution = [
+  { 
+    title: 'Mechanics',
+    description: 'Factory workers were the backbone of the Industrial Revolution, operating machinery in factories that produced goods like textiles, iron, and steel. These workers, including men, women, and even children, often worked long hours in harsh conditions for low wages. The rise of factories marked a significant shift from traditional handcrafting to mass production, and factory workers became a central part of the new industrial economy. Their labor helped drive the rapid growth of industries and cities during this period.'
+  },
+  { 
+    title: 'Engineers',
+    description: 'Engineers were crucial figures during the Industrial Revolution, responsible for designing, building, and improving machines, tools, and infrastructure. Mechanical, civil, and electrical engineers worked on developing steam engines, railways, bridges, and other innovations that powered industrial growth. Engineers like James Watt and George Stephenson made significant contributions that revolutionized transportation and manufacturing. Their work laid the foundation for modern engineering and played a key role in shaping the technological landscape of the 19th century.'
+  },
+  { 
+    title: 'Miners',
+    description: 'Miners were essential workers who extracted coal, iron ore, and other raw materials needed to fuel factories, steam engines, and the overall industrial machinery. Coal mining, in particular, was dangerous and physically demanding work that took place deep underground. Miners faced numerous hazards, such as cave-ins, explosions, and health issues like lung diseases. Despite the risks, mining was a crucial occupation that supported the energy needs of the rapidly expanding industrial economy and powered the development of new technologies and transportation systems.'
+  }
+];
+
+const modernEra = [
+  { 
+    title: 'Software Developers',
+    description: 'Software developers are essential in the modern era, creating the applications and systems that power our digital world. They design, write, and maintain code for various software, from mobile apps and web services to operating systems and video games. As technology has advanced, software development has become a critical field that drives innovation in almost every industry, including healthcare, finance, education, and entertainment. Software developers are central to the digital economy, enabling everything from communication and commerce to data analysis and automation.'
+  },  
+  { 
+    title: 'Scientists',
+    description: 'Data analysts and data scientists are increasingly important in today is data-driven world. They collect, process, and analyze large volumes of data to help organizations make informed decisions. By using statistical methods, machine learning, and data visualization techniques, data professionals can uncover trends, patterns, and insights that drive business strategies, scientific research, and policy-making. Their work is crucial in sectors like marketing, healthcare, finance, and technology, where data is used to optimize operations, predict outcomes, and innovate new solutions.'
+  },
+  { 
+    title: 'Doctor',
+    description: 'Healthcare professionals, such as nurses, medical technicians, and doctors, play a critical role in modern society by providing essential medical care and services. With advancements in medical technology and an increasing focus on public health, the demand for skilled healthcare workers has grown significantly. Nurses and technicians handle patient care, diagnostics, and treatment, often working in hospitals, clinics, and community health centers. Their roles are indispensable in maintaining public health, especially in times of global health crises like the COVID-19 pandemic, where the need for healthcare professionals has been paramount.'
+  }
+]
